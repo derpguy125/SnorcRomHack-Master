17e4cb104b5b1b6b1444f5fbe819c601e0a9d5a8
derpguy125 <derpguy125@gmail.com>
2020-10-27 17:05:58 -0400

    An attempt to make some Title Screen
    afaffaf

--- _anim/Title Screen Sonic.asm
+++ _anim/Title Screen Sonic.asm
@@ -2,5 +2,5 @@
 ; Animation script - Sonic on the title screen
 ; ---------------------------------------------------------------------------
 Ani_TSon:	dc.w byte_A706-Ani_TSon
-byte_A706:	dc.b 7,	0, 1, 2, 3, 4, 5, 6, 7,	afBack, 2
+byte_A706:	dc.b 0, 0, 0, 0, 0, afBack, 0, 0
 		even
