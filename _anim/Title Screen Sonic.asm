637945e2c3f72f9cf56e2728d788e196f69ee5d6
derpguy125 <derpguy125@gmail.com>
2020-10-27 17:16:21 -0400

    Title Screen Good!
    And also 'Snorc Team Presents'

--- _anim/Title Screen Sonic.asm
+++ _anim/Title Screen Sonic.asm
@@ -2,5 +2,5 @@
 ; Animation script - Sonic on the title screen
 ; ---------------------------------------------------------------------------
 Ani_TSon:	dc.w byte_A706-Ani_TSon
-byte_A706:	dc.b 0, 0, 0, 0, 0, afBack, 0, 0
+byte_A706:	dc.b 1, 0, 0, afBack, 1
 		even
